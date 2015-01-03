c91cf14c3367d40bae69406dc8544de19942ed0c
daz <git@feb17.org>
2015-01-02 21:09:10 -0800

    Signed-off-by: daz <git@feb17.org>

--- Playground/playground.fs
+++ Playground/playground.fs
@@ -4,10 +4,14 @@ open System.Net
 open Fss.Pool
 open System.Threading
 
-let template = Template("""{% if 'aardvark'>'zebra'%}hello{%endif%}""")
-let page = template.Render([| ("x",box true) |])
-
-
+let t = Template("{{x[2][4]}}")
+let page = t.Render([| ("x",
+                        box [|
+                            [|1;2;3;4;5;6|];
+                            [|2;4;6;8;10;12|];
+                            [|3;6;9;12;15;18|];
+                            [|4;8;12;16;20;24|]
+                        |])  |])
 printf "page=%s" page
 
 printf "done"
