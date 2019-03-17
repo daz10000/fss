a29feabc522779192a9b4e05d257b68bdaf9860b
Darren Platt <darren@demetrixbio.com>
2019-03-17 15:09:20 -0700

    REL: 1.6.1

--- src/fss_postgres/AssemblyInfo.fs
+++ src/fss_postgres/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("fss_postgres")>]
 [<assembly: AssemblyProductAttribute("fss")>]
 [<assembly: AssemblyDescriptionAttribute("F# Server")>]
-[<assembly: AssemblyVersionAttribute("1.6.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.6.0")>]
+[<assembly: AssemblyVersionAttribute("1.6.1")>]
+[<assembly: AssemblyFileVersionAttribute("1.6.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "fss_postgres"
     let [<Literal>] AssemblyProduct = "fss"
     let [<Literal>] AssemblyDescription = "F# Server"
-    let [<Literal>] AssemblyVersion = "1.6.0"
-    let [<Literal>] AssemblyFileVersion = "1.6.0"
+    let [<Literal>] AssemblyVersion = "1.6.1"
+    let [<Literal>] AssemblyFileVersion = "1.6.1"
